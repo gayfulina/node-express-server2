f04f85a09d42532e80ddfd6aee1c884d26dd7a86
gayfulina <irinmdr@gmail.com>
2020-11-08 11:10:29 +1000

    nodemon

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello', 2 + 2)
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', home);
+app.post('/info', info);
+
+function home(req, res) {
+    res.send('Hello')
+}
+function info(req, res) {
+    res.send('INFO here')
+}
+
+app.listen(PORT, () => {
+    console.log('Example app listening at http:localhost: ${PORT}')
+})
