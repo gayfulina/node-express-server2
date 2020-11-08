b87ec9715d6c1a51cbbeb332c54296425f2c3adb
gayfulina <irinmdr@gmail.com>
2020-11-08 14:47:28 +1000

    eslint

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ app.get('/', home);
 app.post('/info', info);
 
 function home(req, res) {
-    res.send('Hello')
+  res.send('Hello');
 }
-function info(req, res) {
 
-    res.send('INFO here')
+function info(req, res) {
+  const a = 123;
+  const b = Math.random();
+  const c = a + b;
+  res.send('INFO here! ' + c);
 }
 
 app.listen(PORT, () => {
-    console.log('Example app listening at http:localhost: ${PORT}')
-})
+  console.log('Example app listening at http:localhost: ${PORT}');
+});
