ccf0b019fb4a7ff89bc25077130a21c762680d64
gayfulina <irinmdr@gmail.com>
2020-11-09 11:20:16 +1000

    logger

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import home from './home';
 import info from './info';
+import errorHandler from './errorHandler';
 
 const app = express();
 const PORT = 5000;
 
 app.get('/', home);
 app.post('/info', info);
-app.use(apiNotFound);
 
-function apiNotFound(req, res) {
-  res.status(400).json('API not found');
-}
+errorHandler(app);
 
 app.listen(PORT, () => {
   console.log('Example app listening at http:localhost: ${PORT}');
