ea17ddcf849445207cecf41de2381a427e52bf07
gayfulina <irinmdr@gmail.com>
2020-11-09 11:40:57 +1000

    bodyParser2

--- src/parseResponse.js
+++ src/parseResponse.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 
-export default function bodyParse(app) {
+export default function parseResponse(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 }
