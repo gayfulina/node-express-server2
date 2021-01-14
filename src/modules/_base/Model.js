486a7e52a34dad3fb9b1cec71a7860c80bf2a692
gayfulina <irinmdr@gmail.com>
2021-01-13 19:17:51 -0800

    base

--- src/modules/_base/Model.js
+++ src/modules/_base/Model.js
@@ -2,12 +2,15 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const Model = new Schema({
-  title: {
-    type: String,
-    required: true,
+const Model = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
   },
 
-});
+  { timestamps: {} },
+);
 
-export default mongoose.model('User', Model);
+export default mongoose.model('Base', Model);
