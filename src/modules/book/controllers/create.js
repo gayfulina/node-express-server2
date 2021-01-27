17248c4d37c1824beb54e4e355c86174f8063f06
gayfulina <irinmdr@gmail.com>
2021-01-26 16:25:15 -0800

    promises

--- src/modules/book/controllers/create.js
+++ src/modules/book/controllers/create.js
@@ -4,30 +4,30 @@ import Author from '../../author/Model';
 
 export default function create(req, res) {
   const _id = mongoose.Types.ObjectId();
+  const authors = req.body.author;
 
   const newBook = new Book({
     _id,
     name: req.body.name,
-    author: req.body.author,
+    author: authors,
   });
 
-  console.log(req.body.author);
-  //Update author
-
-  req.body.author.forEach((author) => {
-    Author.findById(author)
+  const promises = req.body.author.map(async (author) => {
+    await Author.findById(author)
       .exec()
       .then((doc) => {
-        console.log(doc);
-        doc.book = [...doc.book, _id];
-        doc.save().catch((e) => {
-          throw new Error(e);
-        });
-        res.status(200).json(doc);
+        if (doc) {
+          doc.book = [...doc.book, _id];
+          doc.save().catch((err) => {
+            newBook.author = authors.filter((el) => el !== author);
+            //throw new Error(err);
+          });
+        } else {
+          newBook.author = authors.filter((el) => el !== author);
+        }
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json('Author updateById error');
+        newBook.author = author.filter((el) => el !== author);
       });
   });
 
@@ -35,10 +35,9 @@ export default function create(req, res) {
   newBook
     .save()
     .then(() => {
-      res.status(200).json('Book created');
+      res.status(200).json('Book created!!!');
     })
     .catch((err) => {
-      console.log(err);
       res.status(400).json('Book not created');
     });
 }
