c87a555a5221d0edbe44a3d4fd1cbf4a31f2e205
gayfulina <irinmdr@gmail.com>
2021-01-26 17:59:04 -0800

    promises4

--- src/modules/author/controllers/updateById.js
+++ src/modules/author/controllers/updateById.js
@@ -1,9 +1,54 @@
 import Author from '../Model';
+import Book from '../../book/Model';
 
-export default function updateById(req, res) {
+export default async function updateById(req, res) {
   const authorId = req.params.authorId;
+  const book = req.body.book;
+  const  updateAuthor = {
+      name: req.body.name,
+      book: book,
+  };
 
-  delete req.body.password;
+  await Author.findById(authorId)
+      .exec()
+      .then((doc) => {
+        //-------------------------------------------------------------------------------------------------------------
+        req.body.book.forEach((book) => {
+            if(!doc.book.includes(book)) {
+          Book.findOneAndUpdate({_id: book}, { $addToSet: { author: authorId } })
+              .exec()
+              .then((doc) => {
+                if (doc) {
+                  console.log('books list updated');
+                } else {
+                  console.log('books list not updated')
+                }
+              })
+              .catch((error) => {
+            console.log('error in catch of author find oneAndUpdate')
+            updateAuthor.book = book.filter((el) => el !== book)
+          });
+        }
+      });
+  //---------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------
+doc.book.forEach((book) => {
+  if (!req.body.book.includes(book)) {
+      Book.findOneAndUpdate({_id: book}, {$pull: {book: authorId}})
+          .exec()
+          .then((doc) => {
+              if (doc) {
+                  console.log('books list updated');
+              } else {
+                  console.log('books list not updated')
+              }
+          })
+          .catch((err) => {
+              console.log('error in catch of book find oneAndUpdate')
+              updateAuthor.book = book.filter((el) => el !== book)
+          });
+  }
+});
 
   Author.updateOne({ _id: authorId }, req.body)
     .exec()
@@ -14,4 +59,4 @@ export default function updateById(req, res) {
       console.log(err);
       res.status(400).json('Author updateById error');
     });
-}
+});
