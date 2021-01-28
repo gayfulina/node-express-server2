eb20dbce724ef872830e5768865509f0bfb0e45b
gayfulina <irinmdr@gmail.com>
2021-01-27 19:04:51 -0800

    populate2

--- src/modules/author/controllers/search.js
+++ src/modules/author/controllers/search.js
@@ -2,6 +2,10 @@ import Author from '../Model';
 
 export default function search(req, res) {
   Author.find()
+    .populate({
+      path: 'book',
+      select: 'name createdAt',
+    })
     .exec()
     .then((result) => {
       res.status(200).json(result);
