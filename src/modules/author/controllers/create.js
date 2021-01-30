439f1e564a156e348460c69cccb55f777b8e30f8
gayfulina <irinmdr@gmail.com>
2021-01-30 15:37:05 -0800

    async

--- src/modules/author/controllers/create.js
+++ src/modules/author/controllers/create.js
@@ -4,38 +4,28 @@ import Book from '../../book/Model';
 
 export default async function create(req, res) {
   const _id = mongoose.Types.ObjectId();
-  const books = req.body.book;
+  const book = req.body.book;
+
+  // Check books
+  const bookGetByIdResult = await Book.findById(book)
+    .exec()
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((err) => {
+      console.log(err);
+      //res.status(400).json('Author get all error');
+    });
+
+  console.log(bookGetByIdResult);
   //CREATE NEW AUTHOR
   const newAuthor = new Author({
     _id,
     name: req.body.name,
-    book: books,
+    book: book,
   });
 
-  //check that list of books tat sent exist, otherwise remove from array
-  const promises = req.body.book.map((book) => {
-    Book.findById(book)
-      .exec()
-      .then((doc) => {
-        if (!doc) {
-          newAuthor.books = books.filter((el) => el !== book);
-        } else {
-          //update book to add this author to book
-          Book.findByIdAndUpdate({ _id: book }, { $addToSet: { author: _id } })
-            .exec()
-            .then(() => console.log('updated'))
-            .catch((err) => console.log(err));
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-
-        newAuthor.book = books.filter((el) => el !== book);
-        // res.status(400).json('Book updateById error');
-      });
-  });
-
-  await Promise.all(promises);
   //create Author
   newAuthor
     .save()
--- src/modules/author/controllers/updateById.js
+++ src/modules/author/controllers/updateById.js
@@ -4,51 +4,10 @@ import Book from '../../book/Model';
 export default async function updateById(req, res) {
   const authorId = req.params.authorId;
   const book = req.body.book;
-  const  updateAuthor = {
-      name: req.body.name,
-      book: book,
+  const updateAuthor = {
+    name: req.body.name,
+    book: book,
   };
-
-  await Author.findById(authorId)
-      .exec()
-      .then((doc) => {
-        //-------------------------------------------------------------------------------------------------------------
-        req.body.book.forEach((book) => {
-            if(!doc.book.includes(book)) {
-          Book.findOneAndUpdate({_id: book}, { $addToSet: { author: authorId } })
-              .exec()
-              .then((doc) => {
-                if (doc) {
-                  console.log('books list updated');
-                } else {
-                  console.log('books list not updated')
-                }
-              })
-              .catch((error) => {
-            console.log('error in catch of author find oneAndUpdate')
-            updateAuthor.book = book.filter((el) => el !== book)
-          });
-        }
-      });
-  //---------------------------------------------------------------------------------
-  //---------------------------------------------------------------------------------
-doc.book.forEach((book) => {
-  if (!req.body.book.includes(book)) {
-      Book.findOneAndUpdate({_id: book}, {$pull: {book: authorId}})
-          .exec()
-          .then((doc) => {
-              if (doc) {
-                  console.log('books list updated');
-              } else {
-                  console.log('books list not updated')
-              }
-          })
-          .catch((err) => {
-              console.log('error in catch of book find oneAndUpdate')
-              updateAuthor.book = book.filter((el) => el !== book)
-          });
-  }
-});
 
   Author.updateOne({ _id: authorId }, req.body)
     .exec()
@@ -59,4 +18,4 @@ doc.book.forEach((book) => {
       console.log(err);
       res.status(400).json('Author updateById error');
     });
-});
+}
